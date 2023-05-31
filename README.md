1218a0d47c51c115707ca1a2f2d959cf4b4e8e3c
tanu31195 <tanu31195@users.noreply.github.com>
2023-05-31 15:45:48 +0530

    Added axios implementation

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
+# Harver Assessment
+
 `npm init`
+`npm install fs path request merge-img minimist`
+`npm install axios`
+
+
+## TODO
+
+- [] Find a better package for merge-img
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { writeFile } = require('fs');
 const { join } = require('path');
-const request = require('request');
+const axios = require('axios');
 const mergeImg = require('merge-img');
 const argv = require('minimist')(process.argv.slice(2));
 
@@ -13,53 +13,48 @@ const {
   size = 100,
 } = argv;
 
-const firstReq = {
-  url: `https://cataas.com/cat/says/${greeting}?width=${width}&height=${height}&color=${color}&s=${size}`,
-  encoding: 'binary',
-};
+const firstReq = `https://cataas.com/cat/says/${greeting}?width=${width}&height=${height}&color=${color}&s=${size}`;
+const secondReq = `https://cataas.com/cat/says/${who}?width=${width}&height=${height}&color=${color}&s=${size}`;
 
-const secondReq = {
-  url: `https://cataas.com/cat/says/${who}?width=${width}&height=${height}&color=${color}&s=${size}`,
-  encoding: 'binary',
-};
+axios
+  .get(firstReq, { responseType: 'arraybuffer' })
+  .then((firstRes) => {
+    console.log(`Received response with status: ${firstRes.status}`);
+    const firstBody = Buffer.from(firstRes.data, 'binary');
 
-request.get(firstReq, (err, res, firstBody) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
+    axios
+      .get(secondReq, { responseType: 'arraybuffer' })
+      .then((secondRes) => {
+        console.log(`Received response with status: ${secondRes.status}`);
+        const secondBody = Buffer.from(secondRes.data, 'binary');
 
-  console.log(`Received response with status: ${res.statusCode}`);
+        mergeImg([
+          { src: firstBody, x: 0, y: 0 },
+          { src: secondBody, x: width, y: 0 },
+        ]).then((img) => {
+          img.getBuffer('image/jpeg', (err, buffer) => {
+            if (err) {
+              console.log(err);
+              return;
+            }
 
-  request.get(secondReq, (err, res, secondBody) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
+            const fileOut = join(process.cwd(), `/cat-card.jpg`);
 
-    console.log(`Received response with status: ${res.statusCode}`);
+            writeFile(fileOut, buffer, 'binary', (err) => {
+              if (err) {
+                console.log(err);
+                return;
+              }
 
-    mergeImg([
-      { src: Buffer.from(firstBody, 'binary'), x: 0, y: 0 },
-      { src: Buffer.from(secondBody, 'binary'), x: width, y: 0 },
-    ]).then((img) => {
-      img.getBuffer('image/jpeg', (err, buffer) => {
-        if (err) {
-          console.log(err);
-          return;
-        }
-
-        const fileOut = join(process.cwd(), `/cat-card.jpg`);
-
-        writeFile(fileOut, buffer, 'binary', (err) => {
-          if (err) {
-            console.log(err);
-            return;
-          }
-
-          console.log("The file was saved!");
+              console.log('The file was saved!');
+            });
+          });
         });
+      })
+      .catch((error) => {
+        console.log(error);
       });
-    });
+  })
+  .catch((error) => {
+    console.log(error);
   });
-});
